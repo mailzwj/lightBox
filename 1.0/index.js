960c43ffef4be051bf1bc3eb6844eeac64012c27
wb-zhengwujiang.z <wb-zhengwujiang.z@taobao.com>
2013-07-08 18:59:13 +0800

    修复js名称

--- 1.0/index.js
+++ 1.0/index.js
@@ -11,7 +11,7 @@ KISSY.add(function (S, Node,Base) {
      * @constructor
      * @extends Base
      */
-    function LigntBox(cfg) {
+    function LightBox(cfg) {
         var self = this;
         /**
          * 参数说明
@@ -30,9 +30,9 @@ KISSY.add(function (S, Node,Base) {
         this.init();
 
         //调用父类构造函数
-        LigntBox.superclass.constructor.call(self, cfg);
+        LightBox.superclass.constructor.call(self, cfg);
     }
-    S.extend(LigntBox, Base, /** @lends LigntBox.prototype*/{
+    S.extend(LightBox, Base, /** @lends LigntBox.prototype*/{
         isShow: false,
         currentImg: null,
         log: function(err) { //无用的方法，测试时使用
@@ -249,7 +249,7 @@ KISSY.add(function (S, Node,Base) {
     }, {ATTRS : /** @lends LigntBox*/{
 
     }});
-    return LigntBox;
+    return LightBox;
 }, {requires:['node', 'base']});
 
 
